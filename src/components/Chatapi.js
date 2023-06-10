d2de3a9823224b593c12490125bc1a5903cb7b25
Rostan Rodrigues <foundationai23@gmail.com>
2023-06-10 15:35:08 -0400

    commit

--- src/components/Chatapi.js
+++ src/components/Chatapi.js
@@ -1,46 +1,49 @@
 // api.js
-// import axios from "axios";
-
-// export const sendMessageToServer = async (message) => {
-//   try {
-//     const response = await axios.post("http://localhost:3010", {
-//       message: message,
-//     });
-
-//     return response.data.completion.content;
-//   } catch (error) {
-//     console.error(error);
-//     throw error;
-//   }
-// };
-
-// Chatapi.js
 import axios from "axios";
-const API_KEY = "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";
-
-const instance = axios.create({
-  baseURL: "https://api.openai.com/v1/chat/completions",
-  headers: { Authorization: `Bearer ${API_KEY}` },
-});
 
 export const sendMessageToServer = async (message) => {
   try {
-    const response = await instance.post("", {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: "You are a helpful assistant.",
-        },
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-    });
-    return response.data.choices[0].message.content;
+    // const response = await axios.post("http://localhost:3010", {
+    const response = await axios.post(
+      "https://intense-oasis-44220.herokuapp.com",
+      {
+        message: message,
+      }
+    );
+
+    return response.data.completion.content;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+// import axios from "axios";
+// const API_KEY = "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";
+
+// const instance = axios.create({
+//   baseURL: "https://api.openai.com/v1/chat/completions",
+//   headers: { Authorization: `Bearer ${API_KEY}` },
+// });
+
+// export const sendMessageToServer = async (message) => {
+//   try {
+//     const response = await instance.post("", {
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         {
+//           role: "system",
+//           content: "You are a helpful assistant.",
+//         },
+//         {
+//           role: "user",
+//           content: message,
+//         },
+//       ],
+//     });
+//     return response.data.choices[0].message.content;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
